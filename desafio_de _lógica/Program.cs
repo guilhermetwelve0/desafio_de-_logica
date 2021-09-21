351fd06263fa5fab13c493327e80cc25f54abfb7
Guilherme <guilherme.cabral999@hotmail.com>
2021-09-21 12:40:55 -0300

    Adicionar arquivos de projeto.

---
+++ desafio_de _lógica/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace desafio_de__lógica
+{
+    class Program
+    {
+  
+        static void Main(string[] args)
+        {
+            int Par;
+            int Impar;
+
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("O seu numero é par: " + i );
+                }else
+                {
+                    Console.WriteLine("O seu numero é impar: "+ i );
+                }
+            }
+
+
+        }
+    }
+}
